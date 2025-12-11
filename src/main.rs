7e500601782407e9a8743d256ec21f637745bba8
J M Archer <archer884@zoho.com>
2025-12-11 14:06:17 -0600

    It... works, I guess?

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs,
+    io::{self, Read, Write},
+    process,
+};
+
+use clap::Parser;
+use regex::Regex;
+
+/// Replace -- with mdash in mardown documents
+#[derive(Debug, Parser)]
+#[command(author, about)]
+struct Opts {
+    /// read from path instead of stdin
+    path: Option<String>,
+}
+
+fn main() {
+    if let Err(e) = run(Opts::parse()) {
+        eprintln!("{e}");
+        process::exit(1);
+    }
+}
+
+fn run(opts: Opts) -> io::Result<()> {
+    let content = load_content(&opts)?;
+
+    let comment = Regex::new("<!--.+?-->").unwrap();
+    let content = comment.replace(&content, "");
+    let content = content.replace("--", "—");
+
+    io::stdout().lock().write_all(content.as_bytes())?;
+
+    Ok(())
+}
+
+fn load_content(opts: &Opts) -> io::Result<String> {
+    if let Some(path) = opts.path.as_deref() {
+        return fs::read_to_string(path);
+    }
+
+    let mut content = String::new();
+    io::stdin().lock().read_to_string(&mut content)?;
+    Ok(content)
+}
